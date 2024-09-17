df02178cf1ba3cb3d639cc98bddd455216ee9f77
Perla Hernandez <perherpsic@gmail.com>
2024-09-17 13:26:38 -0600

    descomente codigo

--- limpeza_bd_mortalidad.py
+++ limpeza_bd_mortalidad.py
@@ -32,34 +32,34 @@ agrupacion_anual = agrupacion_anual.sort_values(by=('conteo_por_estado'), ascend
 df_resultado = pd.merge(mortalidad_filtrada, agrupacion_anual, on=['defuncion', 'tipo_aborto', 'tipo_servicio_medico'], how='left')
 
 
-# print(df_resultado)
+print(df_resultado)
 
 
 columnas_exportar = ['estado', 'rango_edad', 'tipo_aborto', 'tipo_servicio_medico', 'defuncion']
 df_resultado.to_csv('muertes_por_aborto.csv', columns=columnas_exportar, index=False)
 
-# conteo_por_año = df_resultado.groupby('defuncion').size()
+conteo_por_año = df_resultado.groupby('defuncion').size()
 
-# df_resultado.groupby('defuncion')['conteo_por_estado'].sum().plot(kind='bar')
-# plt.xlabel('Año')
-# plt.ylabel('Número de casos')
-# plt.title('Número de casos de aborto por año')
-# plt.show()
-
-
-# conteo_por_estado = df_resultado.groupby('estado').size()
-
-# df_resultado.groupby('estado')['conteo_por_estado'].sum().plot(kind='bar')
-# plt.xlabel('estado')
-# plt.ylabel('Número de casos')
-# plt.title('Número de casos de aborto por estado (2002-2022)')
-# plt.show()
+df_resultado.groupby('defuncion')['conteo_por_estado'].sum().plot(kind='bar')
+plt.xlabel('Año')
+plt.ylabel('Número de casos')
+plt.title('Número de casos de aborto por año')
+plt.show()
 
 
-# servicio_medico = df_resultado.groupby('tipo_servicio_medico').size()
+conteo_por_estado = df_resultado.groupby('estado').size()
 
-# df_resultado.groupby('tipo_servicio_medico')['conteo_servicio_medico'].sum().plot(kind='bar')
-# plt.xlabel('tipo_derechohabiencia')
-# plt.ylabel('cantidad')
-# plt.title('DERECHOHABIENCIA (2002-2022)')
-# plt.show()
+df_resultado.groupby('estado')['conteo_por_estado'].sum().plot(kind='bar')
+plt.xlabel('estado')
+plt.ylabel('Número de casos')
+plt.title('Número de casos de aborto por estado (2002-2022)')
+plt.show()
+
+
+servicio_medico = df_resultado.groupby('tipo_servicio_medico').size()
+
+df_resultado.groupby('tipo_servicio_medico')['conteo_servicio_medico'].sum().plot(kind='bar')
+plt.xlabel('tipo_derechohabiencia')
+plt.ylabel('cantidad')
+plt.title('DERECHOHABIENCIA (2002-2022)')
+plt.show()
